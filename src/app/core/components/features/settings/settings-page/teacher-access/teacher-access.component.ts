0548a80433076d772935f76251820ebba16770a0
Emerson Dias <emersonsdias@hotmail.com>
2025-03-29 19:19:14 -0300

    Update teacher access

--- src/app/core/components/features/settings/settings-page/teacher-access/teacher-access.component.ts
+++ src/app/core/components/features/settings/settings-page/teacher-access/teacher-access.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ClassroomDTO, UserDTO, UserRole } from '../../../../../models/api/data-contracts';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ClassroomDTO, TeacherDTO, UserDTO, UserRole } from '../../../../../models/api/data-contracts';
 import { UserService } from '../../../../../services/user/user.service';
 import { RouterModule } from '@angular/router';
 import { ROUTES_KEYS } from '../../../../../../shared/config/routes-keys.config';
@@ -9,6 +9,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
 import { ClassroomService } from '../../../../../services/classroom/classroom.service';
 import { firstValueFrom } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-teacher-access',
@@ -18,6 +20,7 @@ import { firstValueFrom } from 'rxjs';
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
+    NgbAccordionModule,
   ],
   templateUrl: './teacher-access.component.html',
   styleUrl: './teacher-access.component.scss'
@@ -26,19 +29,27 @@ export class TeacherAccessComponent implements OnInit {
 
   classrooms: ClassroomDTO[] = []
   users: UserDTO[] = []
+  usersWithoutClassroom: UserDTO[] = []
   ROUTES_KEYS = ROUTES_KEYS
 
   constructor(
     private _userService: UserService,
     private _classroomService: ClassroomService,
+    private _cdr: ChangeDetectorRef
   ) { }
 
   async ngOnInit(): Promise<void> {
-    this.classrooms = await firstValueFrom(this._classroomService.findAll())
-
-    this._userService.findByRoles([UserRole.TEACHER]).subscribe({
-      next: usersResponse => this.users = usersResponse.sort(this._sortUsers())
+    const [classrooms, users] = await Promise.all([
+      firstValueFrom(this._classroomService.findAll()),
+      firstValueFrom(this._userService.findByRoles([UserRole.TEACHER]))
+    ])
+    this.users = users.sort(this._sortUsers())
+    this.usersWithoutClassroom = [...this.users]
+    classrooms.forEach(classroom => {
+      classroom.teachers = classroom.teachers?.sort(this._sortTeachers())
     })
+    this.classrooms = classrooms
+    this._cdr.detectChanges();
   }
 
   private _sortUsers() {
@@ -49,4 +60,26 @@ export class TeacherAccessComponent implements OnInit {
       return a.active ? -1 : 1
     }
   }
+
+  private _sortTeachers() {
+    return (a: TeacherDTO, b: TeacherDTO) => {
+      if (a.active === b.active) {
+        return (a.person?.name || '').localeCompare(b.person?.name || '')
+      }
+      return a.active ? -1 : 1
+    }
+  }
+
+  getUserByPersonId(personId: string | undefined): UserDTO | undefined {
+    if (!personId) {
+      return undefined
+    }
+    const user = this.users.filter(user => user?.active && user?.person?.id === personId)[0]
+
+    if (user) {
+      this.usersWithoutClassroom = this.usersWithoutClassroom.filter(u => u.id !== user.id);
+    }
+    return user
+  }
+
 }
